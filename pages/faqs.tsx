37dfe7e2ce5d42e991f1f20f5fccaf4615c2ddb7
Leti <leticiapistarelli@gmail.com>
2022-08-18 02:03:43 -0300

    refactor faq styles

--- pages/faqs.tsx
+++ pages/faqs.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import styles from "../styles/Faqs.module.css";
 import Head from "next/head";
 import React from "react";
 import IFaq from "../types/IFaq.type";
@@ -7,16 +8,43 @@ interface IProps {
   data: IFaq[];
 }
 
-const FAQS: NextPage<IProps> = () => {
+const FAQS: NextPage<IProps> = ({ data }) => {
+  const renderDataFaqs = () =>
+    data.map((dataItem) => (
+      <div key={dataItem.id}>
+        <h3 className={styles.question}>
+          <span className={styles.pin}>📌</span>
+          {dataItem.title}
+        </h3>
+        <p className={styles.description}> {dataItem.description}</p>
+      </div>
+    ));
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Preguntas Frecuentes</title>
         <meta name="description" content="Preguntas frecuentes del sitio" />
       </Head>
-      <h1>Preguntas frecuentes</h1>
+      <section className={styles.section}>
+        <h1 className={styles.title}>Preguntas frecuentes</h1>
+        <div className={styles.containerQuestions}>{renderDataFaqs()}</div>
+      </section>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://my-json-server.typicode.com/DH-Esp-Frontend/ctd-fe3-s2-c6-integracion-terminado/db"
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      data: data.faqs,
+    },
+  };
+}
 
 export default FAQS;
